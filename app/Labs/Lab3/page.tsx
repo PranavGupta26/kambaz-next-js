ee0339d4937a3fb25211b5bfe3895ec7667929ce
Pranav Gupta <pranav@Pranavs-MacBook-Air-2.local>
2025-09-15 10:45:51 -0400

    first commit

---
+++ app/Labs/Lab3/page.tsx
@@ -0,0 +1,34 @@
+export default function Lab3() {
+  return (
+    <div id="wd-lab3">
+      <h2>Lab 3</h2>
+      <h3>Inro to type script</h3>
+      <div id="wd-forms">
+        <h4>Form Elements</h4>
+        <form id="wd-text-fields">
+            <h5>Text Fields</h5>
+            <label htmlFor="wd-text-fields-username">Username:</label>
+            <input placeholder="jdoe" id="wd-text-fields-username" /> <br />
+            <label htmlFor="wd-text-fields-password">Password:</label>
+            <input type="password" value="123@#$asd" id="wd-text-fields-password" />
+            <br />
+            <label htmlFor="wd-text-fields-first-name">First name:</label>
+            <input type="text" title="John" id="wd-text-fields-first-name" /> <br />
+            <label htmlFor="wd-text-fields-last-name">Last name:</label>
+            <input type="text" placeholder="Doe"
+                value="Wonderland"
+                title="The last name"
+                id="wd-text-fields-last-name" />
+            {/* copy rest of form elements here  */}
+        </form>
+        </div>
+
+      <div id="wd-h-tag">
+        <h4>Heading Tags</h4>
+        <p>
+            Text documents are often broken up into several sections and subsections. Each section is usually prefaced with a short title or heading that attempts to summarize the topic of the section it precedes. For instance this paragraph is preceded by the heading Heading Tags. The font of the section headings are usually larger and bolder than their subsection headings. This document uses headings to introduce topics such as HTML Documents, HTML Tags, Heading Tags, etc. HTML heading tags can be used to format plain text so that it renders in a browser as large headings. There are 6 heading tags for different sizes: h1, h2, h3, h4, h5, and h6. Tag h1 is the largest heading and h6 is the smallest heading.
+        </p>      
+      </div>
+      {/* do the next exercise here */}
+    </div>
+);}
