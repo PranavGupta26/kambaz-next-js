d8e9d97bc92f6fc5e03ec7e9ac24e284f874cb6d
Pranav Gupta <pranav@Pranavs-MacBook-Air-2.local>
2025-10-29 13:19:11 -0400

    a4 abcdef

--- app/Labs/layout.tsx
+++ app/Labs/layout.tsx
@@ -1,19 +1,26 @@
+"use client";
+
 import { ReactNode } from "react";
 import TOC from "./TOC";
-
+import { Provider } from "react-redux";
+import store from "./Lab4/store";
+// <-- adjust this path to where your store.ts file is
 
 export default function LabsLayout({
- children,
+  children,
 }: Readonly<{ children: ReactNode }>) {
- return (
-   <table>
-     <tbody>
-       <tr>
-         <td valign="top" width="100px">
-           <TOC />
-         </td>
-         <td valign="top">{children}</td>
-       </tr>
-     </tbody>
-   </table>
-);}
+  return (
+    <Provider store={store}>
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top" width="100px">
+              <TOC />
+            </td>
+            <td valign="top">{children}</td>
+          </tr>
+        </tbody>
+      </table>
+    </Provider>
+  );
+}
